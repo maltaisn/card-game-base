8f6a1fcdd6873e8c1e937744576373f35be03111
maltaisn <nicolas.g.maltais@gmail.com>
2019-07-23 14:09:00 -0400

    Updated kotlin, libGDX and gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 buildscript {
-    val kotlinVersion by extra("1.3.31")
-    val gdxVersion by extra("1.9.9")
-    val ktxVersion by extra("1.9.9-b2")
+    val kotlinVersion by extra("1.3.41")
+    val gdxVersion by extra("1.9.10")
+    val ktxVersion by extra("1.9.10-SNAPSHOT")
     val junitVersion by extra("4.12")
     val cardGameVersion by extra("0.0.1")
 
@@ -10,7 +10,7 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.4.1")
+        classpath("com.android.tools.build:gradle:3.4.2")
         classpath(kotlin("gradle-plugin", kotlinVersion))
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.9.18")
     }
@@ -23,6 +23,7 @@ plugins {
 allprojects {
     repositories {
         mavenCentral()
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
         jcenter()
         google()
     }
