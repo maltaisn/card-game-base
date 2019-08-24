409133eda2936cf2bd409e8dfb2e2ab211e71b0e
maltaisn <nicolas.g.maltais@gmail.com>
2019-08-24 14:57:46 -0400

    Updated gradle and kotlin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ buildscript {
     val cardGameVersion by extra("0.0.1")
     val pcardVersion by extra("0.0.1")
 
-    val kotlinVersion by extra("1.3.41")
+    val kotlinVersion by extra("1.3.50")
     val gdxVersion by extra("1.9.10")
     val ktxVersion by extra("1.9.10-SNAPSHOT")
     val junitVersion by extra("4.12")
@@ -12,7 +12,7 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.4.2")
+        classpath("com.android.tools.build:gradle:3.5.0")
         classpath(kotlin("gradle-plugin", kotlinVersion))
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.9.18")
     }
